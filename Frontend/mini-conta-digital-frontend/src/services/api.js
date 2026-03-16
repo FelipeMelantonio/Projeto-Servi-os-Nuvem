07c94387c6af7fb5f50c6bed3a792b682ea4b1de
Felipe Melantonio <felipefmelantonio@gmail.com>
2026-03-16 15:46:01 -0300

    Rota por API

--- Frontend/mini-conta-digital-frontend/src/services/api.js
+++ Frontend/mini-conta-digital-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 // Busca a URL da API de uma variável de ambiente ou usa o padrão local
-const API_BASE_URL = process.env.REACT_APP_API_URL || '/api'; 
+const API_BASE_URL = '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
